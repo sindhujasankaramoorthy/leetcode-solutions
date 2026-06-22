bd8cb26bb4aae57ae9325fc97204971c8d9931ee
Sindhuja Sankaramoorthy <sindhuja.sankaramoorthy.work@gmail.com>
2026-06-22 21:14:58 +0530

    SortColors

---
+++ Arrays/SortColors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int c0=0,c1=0,c2=0;
+
+        for(int x:nums){
+            if(x==0) c0++;
+            else if(x==1) c1++;
+            else c2++;
+        }
+
+        for(int i=0;i<c0;i++){
+            nums[i]=0;
+        }
+        for(int i=c0;i<c0+c1;i++){
+            nums[i]=1;
+        }
+        for(int i=c1+c0;i<c0+c1+c2;i++){
+            nums[i]=2;
+        }
+    }
+}
