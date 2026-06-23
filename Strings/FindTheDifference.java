469c4f94b78dd8917e626a1d85cfb87c9d32d7c6
Sindhuja Sankaramoorthy <sindhuja.sankaramoorthy.work@gmail.com>
2026-06-23 19:59:19 +0530

    389. Find The Difference

---
+++ Strings/FindTheDifference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sum = 0;
+
+        for (char c : t.toCharArray()) {
+            sum += c;
+        }
+
+        for (char c : s.toCharArray()) {
+            sum -= c;
+        }
+
+        return (char) sum;
+    }
+}
