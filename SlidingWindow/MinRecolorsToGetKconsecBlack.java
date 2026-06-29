ce89f385a22054a777684e475e35afc9604e4b4a
Sindhuja Sankaramoorthy <sindhuja.sankaramoorthy.work@gmail.com>
2026-06-29 08:50:11 +0530

    2379. Minimum recolors toget k consecutive black blocks

---
+++ SlidingWindow/MinRecolorsToGetKconsecBlack.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+        int c=0,min;
+        for(int i=0;i<k;i++){
+            if(blocks.charAt(i)=='W') c++;
+        }
+        min=c;
+
+        for(int i=k;i<blocks.length();i++){
+            if(blocks.charAt(i-k)=='W') c--;
+            if(blocks.charAt(i)=='W')c++;
+            min=Math.min(min,c);
+        }
+        return min;
+    }
+}
