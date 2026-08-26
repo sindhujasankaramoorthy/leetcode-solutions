d1b22ed0728c8e042c2e1aaf4129a7127f9acc15
Sindhuja Sankaramoorthy <sindhuja.sankaramoorthy.work@gmail.com>
2026-08-26 13:14:11 +0530

    0052-n-queens-ii solved

---
+++ LeetCode/Hard/0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+
+    int count = 0;
+
+    public int totalNQueens(int n) {
+
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+
+        solve(board, 0, n);
+
+        return count;
+    }
+
+    public void solve(char[][] board, int row, int n) {
+
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(board, row, col, n)) {
+
+                board[row][col] = 'Q';
+
+                solve(board, row + 1, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    public boolean isSafe(char[][] board, int row, int col, int n) {
+
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        for (int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n;
+             i--, j++) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+}
