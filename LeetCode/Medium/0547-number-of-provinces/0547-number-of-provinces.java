adb1483d90d8273e346ff8153058a644e64ff775
Sindhuja Sankaramoorthy <sindhuja.sankaramoorthy.work@gmail.com>
2026-09-25 10:21:48 +0530

    0547-number-of-provinces solved

--- LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,44 +1,24 @@
 class Solution {
-    static boolean[] vis;
+    int n;
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> graph=new ArrayList<>();
-        int n=isConnected.length;
-        vis=new boolean[n+1];
+        n=isConnected.length;
+        boolean[] vis=new boolean[n];
+        int provin=0;
 
-        for(int i=0;i<=n;i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isConnected[i][j] == 1) {
-                    graph.get(i + 1).add(j + 1);
-                }
-            }
-        }
-        int provinces=0;
-
-        for(int i=1;i<=n;i++) {
+        for(int i=0;i<n;i++) {
             if(!vis[i]) {
-                provinces++;
-                bfs(i,graph);
+                provin++;
+                dfs(i,vis,isConnected);
             }
         }
-        return provinces;
+        return provin;
     }
 
-    public void bfs(int n,List<List<Integer>> graph) {
-        Queue<Integer>  q=new LinkedList<>();
-        q.add(n);
-        vis[n]=true;
-
-        while(!q.isEmpty()) {
-            int curr=q.poll();
-            for(int next: graph.get(curr)) {
-                if(!vis[next]) {
-                    vis[next]=true;
-                    q.add(next);
-                }
+    public void dfs(int node, boolean[] vis, int[][] isConnected) {
+        vis[node]=true;
+        for(int i=0;i<n;i++) {
+            if(!vis[i] && isConnected[node][i] == 1) {
+                dfs(i,vis,isConnected);
             }
         }
     }
